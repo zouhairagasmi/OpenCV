4d05fdfcbc1ad2b5e2b02e684588e1b4e1e4e923
Gasmi Zouhaira <zouhaira.gasmi@silicon-austria.com>
2021-10-06 12:33:46 +0200

    read image

--- ColorDetection.py
+++ ColorDetection.py
@@ -1,4 +1,18 @@
 import cv2
 import numpy as np
 
-path = 'Ressources/'
+path = 'Ressources/banana.png'
+img = cv2.imread(path)
+
+imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+
+cv2.namedWindow('Original',cv2.WINDOW_NORMAL)
+cv2.imshow("Original",img)
+cv2.resizeWindow('Original', 500,700)
+
+cv2.namedWindow('HSV',cv2.WINDOW_NORMAL)
+cv2.imshow("HSV",imgHSV)
+cv2.resizeWindow('HSV', 500,700)
+
+cv2.waitKey(0)
